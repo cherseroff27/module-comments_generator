f57218c1fe87413eedeff3116d6f8e960430b698
cherseroff <proffitm1nd@gmail.com>
2024-11-23 02:50:37 +0300

    edited requied install

---
+++ comments_generator/__init__.py
@@ -0,0 +1,1 @@
+from .comments_generator import CommentsGenerator
--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     install_requires=[
         'selenium',
         'pyperclip',
-        'browser_manager @ git+https://github.com/cherseroff27/module-browser_manager.git',
-        'browser_manager @ git+https://github.com/cherseroff27/module-web_element_handler.git',
-        'browser_manager @ git+https://github.com/cherseroff27/module-manual_script_control.git',
+        'browser_manager @ git+https://github.com/cherseroff27/browser_manager.git',
+        'browser_manager @ git+https://github.com/cherseroff27/web_element_handler.git',
+        'browser_manager @ git+https://github.com/cherseroff27/manual_script_control.git',
     ],
     description='Генерация комментариев через ChatGPT.'
                 '(Предполагается автоматизация с помощью модуля browser_manager,'
